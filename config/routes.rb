c0abd19ce0fa9f88a48f96b5768ed4b5aa9e4a4f
Kali Nathalie <kalinathalie@outlook.com>
2019-10-07 06:21:59 -0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/new_cart', to: 'auths#create'
+  post '/cart/update', to: 'purchases#update_cart'
+  post '/cart/add_coupon', to: 'purchases#add_coupon'
+  post '/cart/finish', to: 'purchases#finish'
 end
